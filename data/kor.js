e599740aa5bb6f99ce4966d68089506dcee7ad91
bkbtiger <bkbtiger06@gmail.com>
2026-03-10 17:58:42 +0900

    add 2026 suneung korean answers

---
+++ data/kor.js
@@ -0,0 +1,79 @@
+const korData = [
+
+{
+  id: "kice_kor_2026_11_odd_hw",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "화법과 작문",
+  answers: [
+    3,5,4,1,4,2,5,3,5,5,
+    3,1,1,5,1,2,3,2,4,3,
+    2,5,1,4,4,2,5,1,3,2,
+    4,1,3,3,
+    2,5,3,1,2,5,4,4,3,2,5
+  ]
+},
+
+{
+  id: "kice_kor_2026_11_odd_lm",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "언어와 매체",
+  answers: [
+    3,5,4,1,4,2,5,3,5,5,
+    3,1,1,5,1,2,3,2,4,3,
+    2,5,1,4,4,2,5,1,3,2,
+    4,1,3,3,
+    3,2,4,4,1,2,4,5,2,4,3
+  ]
+},
+
+{
+  id: "kice_kor_2026_11_even_hw",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "화법과 작문",
+  answers: [
+    3,5,4,1,4,2,5,3,5,5,
+    3,1,2,5,1,2,3,1,4,3,
+    2,5,1,4,5,2,4,5,1,3,
+    2,4,1,3,
+    2,5,3,1,2,5,4,4,3,2,5
+  ]
+},
+
+{
+  id: "kice_kor_2026_11_even_lm",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "언어와 매체",
+  answers: [
+    3,5,4,1,4,2,5,3,5,5,
+    3,1,2,5,1,2,3,1,4,3,
+    2,5,1,4,5,2,4,5,1,3,
+    2,4,1,3,
+    3,2,4,4,1,2,4,5,2,4,3
+  ]
+}
+
+];
