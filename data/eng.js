13cd383048f3cd5370a947fb88fe6db1dee2ed17
bkbtiger <bkbtiger06@gmail.com>
2026-03-11 05:25:36 +0900

    24 고3 영어 완

--- data/eng.js
+++ data/eng.js
@@ -358,6 +358,184 @@
     5,5,3,5,1,
     1,2,4,3,2
   ]
+},
+
+{
+  id: "kice_eng_2024_11_odd",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "없음",
+  answers: [
+    2,5,1,3,1,
+    4,3,4,5,2,
+    2,1,5,1,2,
+    3,4,1,2,1,
+    3,2,1,5,4,
+    4,3,3,2,4,
+    5,3,2,5,4,
+    5,3,4,4,1,
+    2,5,2,3,4
+  ]
+},
+{
+  id: "kice_eng_2024_11_even",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "없음",
+  answers: [
+    2,1,5,3,1,
+    4,3,4,5,2,
+    3,1,1,5,2,
+    2,4,1,3,5,
+    3,1,2,5,4,
+    4,3,3,2,4,
+    1,2,2,5,4,
+    5,3,4,4,1,
+    2,5,2,3,4
+  ]
+},
+{
+  id: "office_eng_2024_10_g3",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,2,3,5,5,
+    4,1,3,1,4,
+    4,2,3,1,1,
+    5,4,2,2,1,
+    4,1,3,3,3,
+    5,3,4,2,3,
+    1,2,2,2,3,
+    2,4,5,5,3,
+    1,5,4,5,5
+  ]
+},
+{
+  id: "kice_eng_2024_09_g3",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,1,5,5,1,
+    1,5,3,5,3,
+    2,1,4,2,3,
+    1,3,4,3,3,
+    3,3,4,4,5,
+    2,3,5,5,5,
+    2,2,1,3,4,
+    5,3,5,4,1,
+    1,3,2,2,5
+  ]
+},
+{
+  id: "office_eng_2024_07_g3",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    2,5,2,4,3,
+    4,2,5,4,4,
+    5,3,5,1,1,
+    1,5,1,5,1,
+    1,5,4,2,2,
+    4,4,2,5,4,
+    3,2,3,5,3,
+    3,5,5,5,1,
+    2,4,3,2,5
+  ]
+},
+{
+  id: "kice_eng_2024_06_g3",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,5,3,4,4,
+    3,2,5,2,2,
+    5,2,2,1,3,
+    1,4,3,1,5,
+    2,4,2,1,3,
+    5,5,4,2,4,
+    3,1,1,4,4,
+    1,3,4,5,2,
+    3,5,5,2,5
+  ]
+},
+{
+  id: "office_eng_2024_05_g3",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 5,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,2,1,4,2,
+    4,2,3,5,4,
+    1,3,4,1,5,
+    2,3,5,1,5,
+    5,1,5,4,4,
+    3,4,4,5,5,
+    2,1,1,2,3,
+    3,4,4,3,1,
+    4,5,2,3,3
+  ]
+},
+{
+  id: "office_eng_2024_03_g3",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,5,1,4,2,
+    3,2,5,4,3,
+    3,2,1,3,1,
+    5,3,5,2,2,
+    5,3,1,5,5,
+    4,4,2,4,3,
+    1,1,3,1,4,
+    2,5,4,2,1,
+    3,4,4,4,5
+  ]
 }
+
 
 ];
