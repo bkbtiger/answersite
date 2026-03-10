f25834f56355fec728d05390bb26f3bf0fd27d47
bkbtiger <bkbtiger06@gmail.com>
2026-03-11 00:32:59 +0900

    2025 완

--- data/eng.js
+++ data/eng.js
@@ -36,6 +36,328 @@
     1,5,2,3,4,4,5,5,3,1,
     4,5,5,3,5
   ]
+},
+
+{
+  id: "office_eng_2025_10",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,5,1,4,2,
+    5,3,4,5,4,
+    2,1,3,1,3,
+    1,4,4,2,3,
+    3,1,1,2,3,
+    4,5,5,2,3,
+    1,4,2,5,4,
+    3,2,5,3,4,
+    1,5,5,4,5
+  ]
+},
+
+{
+  id: "kice_eng_2025_09",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    1,2,3,5,4,
+    4,1,4,5,4,
+    2,1,5,3,5,
+    1,3,2,3,2,
+    3,1,1,3,5,
+    5,3,5,2,5,
+    2,3,5,1,4,
+    4,2,3,2,4,
+    1,4,3,4,2
+  ]
+},
+
+{
+  id: "office_eng_2025_07",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,4,1,5,2,
+    2,1,3,5,2,
+    3,2,2,4,3,
+    2,4,1,1,2,
+    3,1,4,1,3,
+    5,4,5,5,5,
+    1,1,3,5,4,
+    4,2,4,4,1,
+    3,3,5,5,3
+  ]
+},
+
+{
+  id: "kice_eng_2025_06",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,5,5,4,4,
+    3,1,4,4,1,
+    5,3,3,1,3,
+    2,4,1,1,2,
+    3,1,3,2,4,
+    2,5,2,3,5,
+    5,1,4,2,4,
+    4,3,4,5,2,
+    1,5,3,3,5
+  ]
+},
+
+{
+  id: "office_eng_2025_05",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 5,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,5,1,4,2,
+    3,1,5,4,2,
+    5,1,2,1,3,
+    1,4,4,2,5,
+    4,5,5,2,4,
+    4,2,3,5,4,
+    2,1,3,4,3,
+    3,4,5,2,5,
+    3,3,5,5,3
+  ]
+},
+
+{
+  id: "office_eng_2025_03",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "영어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    4,5,5,4,2,
+    4,3,4,4,4,
+    1,5,1,1,5,
+    2,4,2,2,5,
+    1,5,1,2,3,
+    3,3,5,3,5,
+    2,1,2,1,3,
+    4,3,3,3,1,
+    2,4,2,3,4
+  ]
+},
+
+{
+  id: "office_eng_2025_10_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2025,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    5,3,3,4,5,
+    2,3,4,5,4,
+    2,1,3,2,1,
+    1,4,2,3,2,
+    4,1,2,5,4,
+    2,2,3,5,5,
+    3,2,1,2,4,
+    4,2,5,4,4,
+    3,5,5,4,4
+  ]
+},
+
+{
+  id: "kice_eng_2025_09_g2",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고2",
+  subject: "영어",
+  year: 2025,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    1,5,3,5,2,
+    3,2,4,4,3,
+    1,2,1,3,5,
+    2,4,1,2,1,
+    2,1,3,1,3,
+    3,2,4,2,3,
+    2,5,5,1,4,
+    5,3,3,4,2,
+    5,4,4,4,5
+  ]
+},
+
+{
+  id: "kice_eng_2025_06_g2",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고2",
+  subject: "영어",
+  year: 2025,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    1,2,5,4,5,
+    3,1,4,3,2,
+    4,1,3,2,1,
+    1,5,2,5,2,
+    5,3,1,5,4,
+    3,5,4,2,3,
+    3,1,3,1,4,
+    2,5,3,2,2,
+    2,4,5,4,4
+  ]
+},
+
+{
+  id: "office_eng_2025_03_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    5,1,1,3,2,
+    2,3,4,5,2,
+    1,2,5,4,3,
+    2,2,5,1,3,
+    4,1,5,5,2,
+    3,3,4,4,4,
+    1,5,2,1,3,
+    2,4,3,3,1,
+    2,4,5,4,4
+  ]
+},
+
+{
+  id: "office_eng_2025_10_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2025,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,1,3,4,1,
+    2,2,4,5,4,
+    3,3,5,2,3,
+    5,4,3,2,1,
+    2,4,1,1,4,
+    3,5,3,2,4,
+    3,1,2,5,4,
+    3,2,3,3,1,
+    2,5,4,5,4
+  ]
+},
+
+{
+  id: "office_eng_2025_09_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2025,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    4,1,5,5,2,
+    4,2,4,4,2,
+    1,5,1,1,1,
+    2,4,3,3,2,
+    1,3,2,2,5,
+    3,3,5,2,5,
+    1,3,1,2,4,
+    2,3,5,5,3,
+    3,3,5,4,4
+  ]
+},
+
+{
+  id: "office_eng_2025_06_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2025,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    3,1,1,5,2,
+    4,2,3,4,4,
+    1,5,2,3,2,
+    5,3,1,1,5,
+    2,1,5,1,4,
+    4,3,4,3,4,
+    2,1,2,5,3,
+    2,5,4,3,1,
+    2,5,4,5,4
+  ]
+},
+
+{
+  id: "office_eng_2025_03_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    4,5,2,3,5,
+    3,2,4,3,2,
+    2,4,1,4,4,
+    5,3,3,3,2,
+    1,1,5,1,4,
+    4,2,2,4,5,
+    1,3,4,1,2,
+    5,5,3,5,1,
+    1,2,4,3,2
+  ]
 }
 
 ];
