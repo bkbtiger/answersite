b081b3588366f88d79e44bd57c9fa8425f393e6b
bkbtiger <bkbtiger06@gmail.com>
2026-03-10 18:10:58 +0900

    add 2026 csat math and english answers

---
+++ data/eng.js
@@ -0,0 +1,41 @@
+const engData = [
+
+{
+  id: "kice_eng_2026_11_odd",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "영어",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "공통",
+  answers: [
+    3,2,1,5,1,4,1,5,4,4,
+    1,2,2,3,3,1,5,2,1,2,
+    2,1,3,2,4,3,4,4,2,5,
+    1,5,4,3,4,4,5,5,3,3,
+    2,5,5,3,5
+  ]
+},
+
+{
+  id: "kice_eng_2026_11_even",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "영어",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "공통",
+  answers: [
+    1,2,3,5,1,4,1,5,4,4,
+    3,2,2,1,1,3,5,2,3,1,
+    2,2,3,3,4,3,4,4,2,5,
+    1,5,2,3,4,4,5,5,3,1,
+    4,5,5,3,5
+  ]
+}
+
+];
---
+++ data/math.js
@@ -0,0 +1,105 @@
+const mathData = [
+
+{
+  id: "kice_math_2026_11_odd_prob",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "확률과 통계",
+  answers: [
+    1,4,5,3,3,2,5,1,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,1,2,5,4,2,977,262
+  ]
+},
+
+{
+  id: "kice_math_2026_11_odd_calc",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "미적분",
+  answers: [
+    1,4,5,3,3,2,5,1,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,4,3,1,2,5,97,11
+  ]
+},
+
+{
+  id: "kice_math_2026_11_odd_geo",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "기하",
+  answers: [
+    1,4,5,3,3,2,5,1,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,1,5,2,4,4,360,221
+  ]
+},
+
+{
+  id: "kice_math_2026_11_even_prob",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "확률과 통계",
+  answers: [
+    1,4,5,3,3,2,1,5,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,1,2,5,4,2,977,262
+  ]
+},
+
+{
+  id: "kice_math_2026_11_even_calc",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "미적분",
+  answers: [
+    1,4,5,3,3,2,1,5,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,4,3,1,2,5,97,11
+  ]
+},
+
+{
+  id: "kice_math_2026_11_even_geo",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2026,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "기하",
+  answers: [
+    1,4,5,3,3,2,1,5,4,3,3,
+    2,5,4,4,9,16,12,15,130,65,457,
+    3,1,5,2,4,4,360,221
+  ]
+}
+
+];
