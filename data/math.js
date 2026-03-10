9647c1c162cfbe88081c9a8bb36fd5c4d46b5128
bkbtiger <bkbtiger06@gmail.com>
2026-03-11 04:42:37 +0900

    24년 수학 완

--- data/math.js
+++ data/math.js
@@ -638,6 +638,629 @@
     5,13,3,20,134,
     68,42,37,121,64
   ]
+},
+
+{
+  id: "kice_math_2024_11_odd_prob",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "확률과 통계",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,4,3,
+    2,1,5,4,2,
+    7,33,96,41,36,
+    16,64,5,3,1,
+    3,3,2,25,19
+  ]
+},
+{
+  id: "kice_math_2024_11_odd_calc",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "미적분",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,4,3,
+    2,1,5,4,2,
+    7,33,96,41,36,
+    16,64,3,4,2,
+    1,1,2,25,17
+  ]
+},
+{
+  id: "kice_math_2024_11_odd_geo",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "기하",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,4,3,
+    2,1,5,4,2,
+    7,33,96,41,36,
+    16,64,3,4,3,
+    1,1,4,107,316
+  ]
+},
+{
+  id: "kice_math_2024_11_even_prob",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "확률과 통계",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,2,3,
+    4,1,5,4,2,
+    7,33,96,41,36,
+    16,64,5,3,1,
+    3,3,2,25,19
+  ]
+},
+{
+  id: "kice_math_2024_11_even_calc",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "미적분",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,2,3,
+    4,1,5,4,2,
+    7,33,96,41,36,
+    16,64,3,4,2,
+    1,1,2,25,17
+  ]
+},
+{
+  id: "kice_math_2024_11_even_geo",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "기하",
+  answers: [
+    5,4,5,2,4,
+    5,3,1,2,3,
+    4,1,5,4,2,
+    7,33,96,41,36,
+    16,64,3,4,3,
+    1,1,4,107,316
+  ]
+},
+
+{
+  id: "office_math_2024_10_prob",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    4,5,2,5,4,
+    2,5,2,1,1,
+    3,1,5,3,4,
+    6,58,12,84,54,
+    15,486,1,3,2,
+    5,4,2,48,61
+  ]
+},
+{
+  id: "office_math_2024_10_calc",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    4,5,2,5,4,
+    2,5,2,1,1,
+    3,1,5,3,4,
+    6,58,12,84,54,
+    15,486,2,5,4,
+    3,1,2,5,40
+  ]
+},
+{
+  id: "office_math_2024_10_geo",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    4,5,2,5,4,
+    2,5,2,1,1,
+    3,1,5,3,4,
+    6,58,12,84,54,
+    15,486,2,1,3,
+    5,4,1,8,20
+  ]
+},
+
+{
+  id: "kice_math_2024_09_prob",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    2,5,4,2,2,
+    2,3,1,5,1,
+    1,2,4,5,1,
+    7,5,29,4,15,
+    31,8,5,1,5,
+    3,4,4,994,93
+  ]
+},
+{
+  id: "kice_math_2024_09_calc",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    2,5,4,2,2,
+    2,3,1,5,1,
+    1,2,4,5,1,
+    7,5,29,4,15,
+    31,8,5,4,4,
+    3,2,3,57,25
+  ]
+},
+{
+  id: "kice_math_2024_09_geo",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    2,5,4,2,2,
+    2,3,1,5,1,
+    1,2,4,5,1,
+    7,5,29,4,15,
+    31,8,3,4,5,
+    3,4,1,63,54
+  ]
+},
+
+{
+  id: "office_math_2024_07_prob",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    4,4,1,5,3,
+    5,2,1,3,2,
+    5,4,1,4,2,
+    11,50,37,2,35,
+    8,96,3,4,5,
+    4,2,2,70,198
+  ]
+},
+{
+  id: "office_math_2024_07_calc",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    4,4,1,5,3,
+    5,2,1,3,2,
+    5,4,1,4,2,
+    11,50,37,2,35,
+    8,96,3,1,2,
+    4,3,2,12,144
+  ]
+},
+{
+  id: "office_math_2024_07_geo",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    4,4,1,5,3,
+    5,2,1,3,2,
+    5,4,1,4,2,
+    11,50,37,2,35,
+    8,96,3,1,5,
+    2,4,4,90,111
+  ]
+},
+
+{
+  id: "kice_math_2024_06_prob",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    4,5,3,3,5,
+    1,4,1,3,5,
+    5,3,3,4,2,
+    7,23,2,16,24,
+    15,231,3,2,4,
+    3,1,1,6,108
+  ]
+},
+{
+  id: "kice_math_2024_06_calc",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    4,5,3,3,5,
+    1,4,1,3,5,
+    5,3,3,4,2,
+    7,23,2,16,24,
+    15,231,2,3,3,
+    2,2,4,55,25
+  ]
+},
+{
+  id: "kice_math_2024_06_geo",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    4,5,3,3,5,
+    1,4,1,3,5,
+    5,3,3,4,2,
+    7,23,2,16,24,
+    15,231,4,2,4,
+    3,2,3,25,10
+  ]
+},
+
+{
+  id: "office_math_2024_05_prob",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 5,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    4,2,3,3,4,
+    5,2,5,1,5,
+    1,5,1,2,4,
+    5,7,16,11,25,
+    64,114,4,2,1,
+    5,3,2,75,40
+  ]
+},
+{
+  id: "office_math_2024_05_calc",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 5,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    4,2,3,3,4,
+    5,2,5,1,5,
+    1,5,1,2,4,
+    5,7,16,11,25,
+    64,114,1,3,4,
+    4,5,2,40,138
+  ]
+},
+{
+  id: "office_math_2024_05_geo",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 5,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    4,2,3,3,4,
+    5,2,5,1,5,
+    1,5,1,2,4,
+    5,7,16,11,25,
+    64,114,3,4,1,
+    2,3,4,24,36
+  ]
+},
+
+{
+  id: "office_math_2024_03_prob",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "확률과 통계",
+  answers: [
+    5,3,2,1,4,
+    5,1,2,3,4,
+    2,5,2,3,3,
+    3,16,113,80,36,
+    13,2,1,5,2,
+    4,3,2,117,90
+  ]
+},
+{
+  id: "office_math_2024_03_calc",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "미적분",
+  answers: [
+    5,3,2,1,4,
+    5,1,2,3,4,
+    2,5,2,3,3,
+    3,16,113,80,36,
+    13,2,1,3,5,
+    4,2,3,270,84
+  ]
+},
+{
+  id: "office_math_2024_03_geo",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "수학",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "기하",
+  answers: [
+    5,3,2,1,4,
+    5,1,2,3,4,
+    2,5,2,3,3,
+    3,16,113,80,36,
+    13,2,3,2,5,
+    4,1,3,29,150
+  ]
+},
+
+{
+  id: "office_math_2024_10_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "수학",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    3,5,4,4,1,
+    2,3,5,1,3,
+    2,1,5,2,5,
+    4,1,2,3,4,
+    3,8,7,34,6,
+    385,54,22,48,75
+  ]
+},
+
+{
+  id: "office_math_2024_09_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "수학",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    5,4,1,3,4,
+    4,2,4,2,2,
+    1,2,5,3,1,
+    1,3,2,3,4,
+    3,2,12,385,20,
+    36,271,40,11,28
+  ]
+},
+
+{
+  id: "office_math_2024_06_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "수학",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    4,2,5,4,1,
+    3,5,1,2,3,
+    4,3,3,1,3,
+    1,2,4,5,4,
+    2,5,65,29,12,
+    22,18,144,28,47
+  ]
+},
+
+{
+  id: "office_math_2024_03_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "수학",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    3,4,1,2,5,
+    3,1,4,3,5,
+    5,2,2,5,1,
+    3,4,1,4,2,
+    4,20,30,6,24,
+    12,510,189,5,36
+  ]
+},
+
+{
+  id: "office_math_2024_10_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "수학",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    1,1,4,5,1,
+    4,2,2,2,1,
+    3,3,5,5,5,
+    3,2,5,4,4,
+    5,3,15,11,8,
+    5,27,25,154,36
+  ]
+},
+
+{
+  id: "office_math_2024_09_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "수학",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    3,2,1,3,1,
+    4,5,5,2,2,
+    1,4,3,5,2,
+    3,4,1,3,4,
+    5,13,9,3,18,
+    20,32,30,67,144
+  ]
+},
+
+{
+  id: "office_math_2024_06_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "수학",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    3,4,4,1,3,
+    2,4,5,3,1,
+    2,5,2,4,5,
+    3,5,1,4,1,
+    2,6,4,29,23,
+    8,11,20,13,154
+  ]
+},
+
+{
+  id: "office_math_2024_03_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "수학",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "공통",
+  answers: [
+    2,5,1,3,4,
+    1,5,1,4,2,
+    2,3,4,3,3,
+    5,5,4,2,1,
+    2,6,15,126,32,
+    578,153,29,9,91
+  ]
 }
 
 ];
