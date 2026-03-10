708bb08d04aae4a9fae75d6f60904bba04842907
bkbtiger <bkbtiger06@gmail.com>
2026-03-10 23:05:40 +0900

    국어 2025 완

--- data/kor.js
+++ data/kor.js
@@ -306,6 +306,236 @@
     5,4,2,2,1,
     4,3,4,5,4,3
   ]
+},
+
+{
+  id: "office_kor_2025_03_hw",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    2,4,4,2,3,
+    3,2,5,1,5,
+    4,5,3,2,1,
+    1,5,4,2,5,
+    1,1,3,4,5,
+    1,4,1,1,4,
+    1,1,5,2,
+    3,5,4,2,5,
+    3,1,3,2,2,4
+  ]
+},
+
+{
+  id: "office_kor_2025_03_lm",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    2,4,4,2,3,
+    3,2,5,1,5,
+    4,5,3,2,1,
+    1,5,4,2,5,
+    1,1,3,4,5,
+    1,4,1,1,4,
+    1,1,5,2,
+    2,4,4,3,5,
+    1,4,3,5,3,3
+  ]
+},
+
+{
+  id: "office_kor_2025_10_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2025,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,4,5,5,4,
+    5,5,4,4,4,
+    1,4,2,1,3,
+    2,3,4,5,5,
+    3,5,5,1,4,
+    3,1,1,5,1,
+    2,3,5,5,1,
+    5,1,2,3,4,
+    3,5,2,2,4
+  ]
+},
+
+{
+  id: "office_kor_2025_09_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2025,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    1,4,3,2,4,
+    4,5,3,5,2,
+    1,2,2,1,4,
+    5,3,2,1,4,
+    1,1,2,5,4,
+    5,1,2,3,5,
+    2,4,5,2,4,
+    2,3,3,5,4,
+    3,5,1,3,4
+  ]
+},
+
+{
+  id: "office_kor_2025_06_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2025,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,3,4,4,5,
+    1,4,1,3,4,
+    5,3,3,1,2,
+    3,4,3,1,5,
+    1,2,1,4,2,
+    3,5,5,1,1,
+    2,2,5,4,4,
+    5,5,2,1,3,
+    2,2,2,4,5
+  ]
+},
+
+{
+  id: "office_kor_2025_03_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    1,5,4,2,5,
+    2,1,5,3,2,
+    3,3,1,4,2,
+    3,4,2,3,5,
+    1,2,2,3,1,
+    5,1,3,1,5,
+    2,4,3,5,4,
+    3,4,2,2,4,
+    5,4,3,1,4
+  ]
+},
+
+{
+  id: "office_kor_2025_10_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2025,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    2,4,5,4,5,
+    5,2,3,3,5,
+    3,2,3,5,4,
+    1,5,1,5,2,
+    1,4,2,5,2,
+    3,4,4,2,4,
+    1,4,3,2,3,
+    3,2,5,1,4,
+    2,3,5,4,5
+  ]
+},
+
+{
+  id: "office_kor_2025_09_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2025,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    1,4,5,5,2,
+    3,4,3,1,4,
+    2,1,4,4,3,
+    5,2,2,2,5,
+    3,4,3,1,1,
+    1,2,5,1,2,
+    2,3,4,5,1,
+    4,5,3,5,3,
+    3,5,2,4,3
+  ]
+},
+
+{
+  id: "office_kor_2025_06_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2025,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    1,4,5,2,2,
+    4,1,4,5,2,
+    1,2,1,2,5,
+    3,4,5,2,1,
+    3,3,1,5,1,
+    4,5,5,3,2,
+    5,5,3,2,3,
+    3,3,4,5,3,
+    4,3,2,5,3
+  ]
+},
+
+{
+  id: "office_kor_2025_03_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2025,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,1,2,4,4,
+    2,1,3,5,3,
+    5,1,5,1,2,
+    4,2,5,3,2,
+    5,2,1,1,5,
+    4,5,2,2,3,
+    3,5,4,5,3,
+    5,4,3,1,3,
+    1,4,2,2,4
+  ]
 }
 
 ];
