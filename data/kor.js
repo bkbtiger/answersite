41f881048f4f71bacd0491192105b1badab530d3
bkbtiger <bkbtiger06@gmail.com>
2026-03-12 05:01:28 +0900

    2023 국어 완

--- data/kor.js
+++ data/kor.js
@@ -1016,6 +1016,548 @@
     3,5,3,4,5,
     3,3,3,5,3
   ]
+},
+
+{
+  id: "kice_kor_2023_11_odd_hw",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "화법과 작문",
+  answers: [
+    5,3,1,5,3,2,2,3,1,5,
+    2,3,1,4,4,5,4,2,1,3,
+    5,2,1,3,3,2,5,1,4,1,
+    2,2,3,4,
+    4,5,4,4,5,1,3,3,2,5,1
+  ]
+},
+
+{
+  id: "kice_kor_2023_11_odd_lm",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "홀수형",
+  selectedSubject: "언어와 매체",
+  answers: [
+    5,3,1,5,3,2,2,3,1,5,
+    2,3,1,4,4,5,4,2,1,3,
+    5,2,1,3,3,2,5,1,4,1,
+    2,2,3,4,
+    4,4,1,3,4,2,1,5,3,3,5
+  ]
+},
+
+{
+  id: "kice_kor_2023_11_even_hw",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "화법과 작문",
+  answers: [
+    1,3,1,5,3,2,2,3,1,5,
+    2,3,1,2,4,5,2,4,1,3,
+    5,2,1,3,3,4,5,5,4,1,
+    2,2,3,4,
+    4,5,4,2,5,1,3,3,4,5,1
+  ]
+},
+
+{
+  id: "kice_kor_2023_11_even_lm",
+  provider: "평가원",
+  examType: "수능",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "짝수형",
+  selectedSubject: "언어와 매체",
+  answers: [
+    1,3,1,5,3,2,2,3,1,5,
+    2,3,1,2,4,5,2,4,1,3,
+    5,2,1,3,3,4,5,5,4,1,
+    2,2,3,4,
+    4,4,1,3,2,4,1,5,3,3,5
+  ]
+},
+
+{
+  id: "office_kor_2023_10_g3_hw",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    4,2,2,3,2,
+    5,1,5,2,5,
+    5,4,3,1,3,
+    2,2,5,2,3,
+    4,3,4,4,1,
+    4,4,1,1,5,
+    1,3,3,5,
+    1,4,1,3,4,
+    5,2,3,1,2,4
+  ]
+},
+
+{
+  id: "office_kor_2023_10_g3_lm",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    4,2,2,3,2,
+    5,1,5,2,5,
+    5,4,3,1,3,
+    2,2,5,2,3,
+    4,3,4,4,1,
+    4,4,1,1,5,
+    1,3,3,5,
+    3,2,4,1,5,
+    2,1,1,3,4,4
+  ]
+},
+
+{
+  id: "kice_kor_2023_09_g3_hw",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    2,3,3,3,5,
+    4,1,5,4,5,
+    2,4,5,3,5,
+    5,1,5,2,3,
+    3,2,4,4,3,
+    2,4,1,4,5,
+    4,2,4,1,
+    2,4,3,4,5,
+    1,5,1,3,5,5
+  ]
+},
+
+{
+  id: "kice_kor_2023_09_g3_lm",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    2,3,3,3,5,
+    4,1,5,4,5,
+    2,4,5,3,5,
+    5,1,5,2,3,
+    3,2,4,4,3,
+    2,4,1,4,5,
+    4,2,4,1,
+    4,3,5,2,1,
+    5,2,5,1,3,4
+  ]
+},
+
+{
+  id: "office_kor_2023_07_hw",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    4,4,2,5,1,
+    4,3,4,5,2,
+    3,4,1,5,2,
+    2,5,1,5,3,
+    4,1,4,3,2,
+    1,4,2,3,1,
+    1,3,5,2,
+    1,4,4,3,5,
+    2,5,4,4,5,5
+  ]
+},
+
+{
+  id: "office_kor_2023_07_lm",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 7,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    4,4,2,5,1,
+    4,3,4,5,2,
+    3,4,1,5,2,
+    2,5,1,5,3,
+    4,1,4,3,2,
+    1,4,2,3,1,
+    1,3,5,2,
+    1,4,4,5,4,
+    1,5,5,5,2,3
+  ]
+},
+
+{
+  id: "kice_kor_2023_06_hw",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    2,5,1,2,4,
+    5,5,2,1,4,
+    3,1,3,1,2,
+    3,4,1,2,4,
+    5,5,4,1,3,
+    4,5,3,4,2,
+    4,2,5,3,
+    1,5,2,4,3,
+    5,1,4,2,5,
+    3
+  ]
+},
+
+{
+  id: "kice_kor_2023_06_lm",
+  provider: "평가원",
+  examType: "모의평가",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    2,5,1,2,4,
+    5,5,2,1,4,
+    3,1,3,1,2,
+    3,4,1,2,4,
+    5,5,4,1,3,
+    4,5,3,4,2,
+    4,2,5,3,
+    3,5,2,4,4,
+    2,1,5,5,3,
+    1
+  ]
+},
+
+{
+  id: "office_kor_2023_04_hw",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 4,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    5,5,4,5,5,
+    3,3,3,1,5,
+    4,4,4,2,3,
+    4,2,3,2,5,
+    4,3,4,1,4,
+    4,1,4,5,3,
+    3,2,5,3,
+    5,4,3,2,5,
+    5,2,4,1,4,
+    3
+  ]
+},
+
+{
+  id: "office_kor_2023_04_lm",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 4,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    5,5,4,5,5,
+    3,3,3,1,5,
+    4,4,4,2,3,
+    4,2,3,2,5,
+    4,3,4,1,4,
+    4,1,4,5,3,
+    3,2,5,3,
+    2,1,3,5,4,
+    2,2,2,4,5,
+    4
+  ]
+},
+
+{
+  id: "office_kor_2023_03_hw",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "화법과 작문",
+  answers: [
+    3,5,4,4,2,
+    5,4,1,4,1,
+    2,5,1,2,3,
+    5,2,4,5,3,
+    5,1,3,3,2,
+    1,3,1,2,5,
+    3,4,2,3,
+    2,5,5,3,4,
+    4,1,1,1,2,
+    4
+  ]
+},
+
+{
+  id: "office_kor_2023_03_lm",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고3",
+  subject: "국어",
+  year: 2023,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "언어와 매체",
+  answers: [
+    3,5,4,4,2,
+    5,4,1,4,1,
+    2,5,1,2,3,
+    5,2,4,5,3,
+    5,1,3,3,2,
+    1,3,1,2,5,
+    3,4,2,3,
+    4,3,1,4,5,
+    2,4,5,1,2,
+    3
+  ]
+},
+
+{
+  id: "office_kor_2023_11_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,5,4,5,1,
+    5,5,3,5,1,
+    3,1,2,3,1,
+    5,4,2,3,4,
+    3,2,3,5,4,
+    5,3,4,2,1,
+    5,5,5,4,4,
+    2,1,1,3,1,
+    4,1,4,5,2
+  ]
+},
+
+{
+  id: "office_kor_2023_09_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2023,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,1,1,1,3,
+    4,2,4,3,5,
+    2,4,3,1,1,
+    4,1,4,2,3,
+    5,1,2,4,5,
+    5,2,4,4,4,
+    3,2,5,2,5,
+    2,2,5,3,5,
+    2,2,3,3,1
+  ]
+},
+
+{
+  id: "office_kor_2023_06_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2023,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,4,5,1,3,
+    3,4,2,4,1,
+    4,2,2,5,2,
+    1,2,2,4,3,
+    4,3,5,4,3,
+    5,3,5,5,1,
+    5,1,4,5,2,
+    1,4,1,2,1,
+    3,1,2,3,5
+  ]
+},
+
+{
+  id: "office_kor_2023_03_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "국어",
+  year: 2023,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,5,3,3,3,
+    5,3,2,2,1,
+    1,1,3,4,2,
+    2,1,5,5,2,
+    2,3,3,5,4,
+    5,1,4,5,4,
+    1,3,4,2,1,
+    5,4,2,3,4,
+    2,4,1,2,3
+  ]
+},
+
+{
+  id: "office_kor_2023_11_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2023,
+  month: 11,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    2,3,4,4,2,
+    4,5,3,5,4,
+    1,2,4,2,1,
+    4,3,1,5,3,
+    2,2,2,3,1,
+    3,2,3,3,1,
+    1,4,5,4,5,
+    5,4,4,3,4,
+    4,5,3,5,4
+  ]
+},
+
+{
+  id: "office_kor_2023_09_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2023,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,4,5,2,4,
+    3,1,2,4,4,
+    5,3,1,2,5,
+    1,4,2,3,5,
+    3,4,5,4,2,
+    4,1,5,5,1,
+    5,2,3,3,1,
+    3,2,1,3,2,
+    5,1,4,3,2
+  ]
+},
+
+{
+  id: "office_kor_2023_06_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2023,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,2,2,4,3,
+    4,5,2,5,3,
+    1,1,5,5,3,
+    1,1,2,3,2,
+    1,4,3,5,3,
+    4,1,1,4,4,
+    5,5,3,3,4,
+    2,5,2,2,4,
+    5,3,4,1,3
+  ]
+},
+
+{
+  id: "office_kor_2023_03_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "국어",
+  year: 2023,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    2,2,1,5,3,
+    3,5,3,4,1,
+    5,4,3,5,3,
+    2,4,3,5,1,
+    1,2,4,4,4,
+    1,4,2,5,4,
+    5,2,2,1,2,
+    5,5,1,2,3,
+    3,3,1,4,5
+  ]
 }
 
 ];
