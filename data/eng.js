b4bb3fc7cbe429e05026a7bc6010ad0269a6a3ae
bkbtiger <bkbtiger06@gmail.com>
2026-03-12 04:14:54 +0900

    2024 국영수 완

--- data/eng.js
+++ data/eng.js
@@ -535,6 +535,190 @@
     2,5,4,2,1,
     3,4,4,4,5
   ]
+},
+
+{
+  id: "office_eng_2024_10_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    2,1,3,5,1,
+    4,5,4,5,4,
+    2,2,4,3,1,
+    2,3,4,1,1,
+    4,4,3,3,5,
+    3,4,2,5,4,
+    5,1,2,4,4,
+    2,5,3,5,1,
+    5,4,3,3,2
+  ]
+},
+
+{
+  id: "office_eng_2024_09_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,3,3,4,5,
+    2,5,4,5,3,
+    2,1,2,3,1,
+    2,4,1,2,5,
+    1,5,2,4,4,
+    4,5,4,4,5,
+    3,1,1,5,3,
+    5,3,2,4,1,
+    1,4,2,3,3
+  ]
+},
+
+{
+  id: "office_eng_2024_06_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,1,1,5,4,
+    1,3,3,5,1,
+    4,5,2,4,5,
+    2,3,2,1,2,
+    1,4,2,4,3,
+    5,5,3,5,4,
+    1,3,1,2,3,
+    2,3,4,3,5,
+    1,5,2,3,3
+  ]
+},
+
+{
+  id: "office_eng_2024_03_g2",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고2",
+  subject: "영어",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,1,1,4,5,
+    3,2,5,5,1,
+    1,1,2,2,2,
+    5,3,2,5,1,
+    4,2,5,5,5,
+    4,3,4,4,3,
+    3,2,3,4,3,
+    5,2,3,2,1,
+    1,4,4,4,2
+  ]
+},
+
+{
+  id: "office_eng_2024_10_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2024,
+  month: 10,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    4,2,5,5,3,
+    5,1,4,4,3,
+    1,2,1,5,2,
+    3,4,5,1,2,
+    4,5,5,3,3,
+    3,4,5,4,3,
+    2,2,1,2,4,
+    3,1,4,2,1,
+    1,3,2,4,3
+  ]
+},
+
+{
+  id: "office_eng_2024_09_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2024,
+  month: 9,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,3,5,4,2,
+    3,4,3,4,4,
+    1,2,1,1,1,
+    5,3,5,5,2,
+    5,3,4,1,4,
+    2,5,3,5,4,
+    2,1,1,4,4,
+    2,4,3,2,4,
+    3,3,5,3,2
+  ]
+},
+
+{
+  id: "office_eng_2024_06_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2024,
+  month: 6,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    3,1,3,5,4,
+    3,5,2,5,4,
+    1,3,5,2,3,
+    3,4,2,2,1,
+    2,1,5,1,3,
+    5,4,5,3,5,
+    2,2,5,1,3,
+    3,2,3,4,1,
+    1,4,5,2,4
+  ]
+},
+
+{
+  id: "office_eng_2024_03_g1",
+  provider: "교육청",
+  examType: "모의고사",
+  grade: "고1",
+  subject: "영어",
+  year: 2024,
+  month: 3,
+  paperType: "공통",
+  selectedSubject: "없음",
+  answers: [
+    5,2,5,5,2,
+    3,5,4,4,3,
+    3,5,2,1,1,
+    1,4,3,1,2,
+    1,3,1,5,3,
+    4,4,4,2,3,
+    2,3,3,5,4,
+    4,3,1,2,1,
+    1,4,5,4,2
+  ]
 }
 
 
